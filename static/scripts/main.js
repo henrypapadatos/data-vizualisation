19810d149148a66df4224d4319af9c571bfff834
Hain Luud <hain.luud@epfl.ch>
2023-04-01 15:39:59 +0200

    Added hover functionality and heat to 2D map

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -7,26 +7,37 @@ function whenDocumentLoaded(action) {
 	}
 }
 
-function draw2DMap() {
+function draw2DMap(gni_per_capita) {
 	// Code and tutorial from https://bost.ocks.org/mike/map/
 	console.log("draw2DMap");
+	let input = 100000;
+	let country_ratios = {};
 
-	const margin = {top: 20, right: 10, bottom: 40, left: 100};
-	const width = 600 - margin.left - margin.right;
-    const height = 400 - margin.top - margin.bottom;
+	// const margin = {top: 20, right: 10, bottom: 40, left: 100};
+	const margin = {top: 0, right: 0, bottom: 0, left: 0};
+	const width = 1100 - margin.left - margin.right;
+    const height = 600 - margin.top - margin.bottom;
 
-
-	d3.json("static/data/countries-50m.json").then((world) => {
+	d3.json("static/data/updated-countries-50m.json").then((world) => {
 		const land = topojson.feature(world, world.objects.countries);
 		const projection = d3.geoNaturalEarth1().fitSize([width, height], land);
+		
+		const mapContainer = d3.select("#root").append("div").attr("id", "map-container");
 
-		let svg = d3.select("#root")
+		// Adding the svg element
+		let svg = d3.select("#map-container")
 					.append("svg")
 					.attr("width", width)
 					.attr("height", height);
 		
+		//let infobox = d3.select("#map-container").append("div").attr("id", "info-box");
+		
 		// Adding a group element for the map
 		let map = svg.append("g").attr("class", "map");
+
+		// Adding legend color scale
+		var color = d3.scaleLinear().range(["white", "red"]);
+		color.domain([1, 50]);
 
 		// Adding a background rectangle
 		map.append("rect")
@@ -39,31 +50,56 @@ function draw2DMap() {
 			.data(land.features)
 		  	.enter()
 			.append("path")
-			.attr("class", function(d) { return "country " + d.properties.name;  }) 
-			.attr("d", d3.geoPath().projection(projection));
+			.attr("class", function(country) { return "country " + country.properties.code;  }) 
+			.attr("d", d3.geoPath().projection(projection))
+			.attr("fill", function(country) { 
+				// Color based on the proportion of your average income compared to other countries
+				
+				if (!(country.properties.code in gni_per_capita)) {
+					return "white";
+				}
+				let ppp_avg_income = gni_per_capita[country.properties.code]["income"]
+				let ratio = input /  ppp_avg_income;
+				
+				ratio = Math.round(ratio)
+				country_ratios[country.properties.code] = ratio;
+
+				if (ratio < 1) {
+					ratio = 1;
+				}
+				else if (ratio > 50) {
+					ratio = 50;
+				}
+				return color(ratio); 
+			})
+			.on("mouseover", function(country) {
+				console.log(country.target.__data__.properties.code);
+				// Update the information box with the country name
+				d3.select("#map-container")
+					.append("div")
+					.attr("id", "info-box")
+					.html(() => {
+						console.log(country.target.__data__.properties.code)
+						if (country_ratios[country.target.__data__.properties.code] == undefined) {
+							return `<h3>${country.target.__data__.properties.name}</h3><p>Has no data</p>`
+						}
+						return `<h3>${country.target.__data__.properties.name}</h3><p>Has ${country_ratios[country.target.__data__.properties.code]} times lower average income that your</p>`	
+					})
+					.style("opacity", 1)
+					.attr('pointer-events', 'none')
+					.style("left", (country.x + 50 + "px"))
+					.style("top", (country.y +"px"));
+			})
+			.on("mouseout", function(country) {
+				// Hide the information box
+				d3.select('#info-box').remove();
+			});
 
 		// Adding country borders
 		map.append("path")
 			.datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
 			.attr("d", d3.geoPath().projection(projection))
 			.attr("class", "country-border");
-
-		var legend_x = width 
-		var legend_y = height 
-			
-		const legend = svg.append("g").attr("class", "legend").attr("transform", "translate(" + legend_x + "," + legend_y + ")");
-		legend
-			.append("rect")
-			.attr("width", 100)
-			.attr("height", 20)
-			.attr("fill", "yellow")
-			.attr("stroke", "black");
-
-		legend
-			.append("text")
-			.attr("x", 10)
-			.attr("y", 15)
-			.text("Legend");
 
 		// https://observablehq.com/@harrystevens/introducing-d3-geo-scale-bar
 		map.call(d3.zoom()
@@ -151,5 +187,7 @@ function draw3DMap() {
 		// );
 
 whenDocumentLoaded(() => {
-
+	// d3.json("static/data/gni_per_capita.json").then( data => {
+	// 	draw2DMap(data);
+	// });
 });
