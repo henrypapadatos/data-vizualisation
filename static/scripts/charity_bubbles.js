ec1d3a3a600f07a40a5a412756ade0ffb3ed08a3
TanguyMarbot <tanguymarbot@hotmail.com>
2023-05-25 20:12:48 +0200

    improved the charity

--- static/scripts/charity_bubbles.js
+++ static/scripts/charity_bubbles.js
@@ -6,8 +6,9 @@ function drawCharityBubbles() {
 function bubbles(){
     var width = 1000, height = 1000;
     const bubblesContainer = d3.select("#bubbles-container")
-    
-    
+    let isTransitioning = false;
+    const TRANS_DURATION = 1000;
+
     var svg = bubblesContainer
         .append("svg")
         .attr("height", height)
@@ -17,10 +18,10 @@ function bubbles(){
     
 
     const cornerBoxData = [
-        { className: 'top-left', top: height / 8, left: width / 8, text: 'Global Health' },
-        { className: 'top-right', top: height / 8, left: width * (3/4), text: 'Biological Risks' },
-        { className: 'bottom-left', top:  height * (5/8), left: width / 8, text: 'Animal Welfare' },
-        { className: 'bottom-right', top: height * (5/8), left:  width * (5/8), text: 'Climate Crisis' }
+        { className: 'top-left', top: height * (1/6), left: width * (1/5), text: 'Global Health and Development' },
+        { className: 'top-right', top: height * (1/6), left: width * (3.5/5), text: 'Catastrophic Risks' },
+        { className: 'bottom-left', top:  height * (4/6), left: width * (1/5), text: 'Animal Welfare' },
+        { className: 'bottom-right', top: height * (4/6), left:  width * (3.5/5), text: 'Funds' }
     ];
     
     bubblesContainer.selectAll('.cause_area-box')
@@ -31,41 +32,64 @@ function bubbles(){
         .style('top', d => d.top ? d.top + 'px' : null)
         .style('left', d => d.left ? d.left + 'px' : null)
         .style('background-color', 'white')
-        .style('border', '1px solid #ba2934')
+        .style('border', '2px solid #ba2934')
         .style('padding', '10px')
         .style('font', 'sans-serif')
         .style('font-size', '20px')
         .style('opacity', '1.0') 
+        .style("border-radius", "0.5rem")
+
         .text(d => d.text);
 
     const buttons = [
-        {id : 'button_cause', text : 'Cause Separation'},
+        {id : 'button_separation', text : 'Cause Separation'},
         {id : 'combine', text : 'Combine'},
         //{id: 'All', text : 'All'},
-        {id : 'button_Animal_welfare', text : 'Animal Welfare'},
-        {id : 'button_Human_wellbeing', text : 'Global Health'},
-        {id : 'button_Creating_a_better_future', text : 'Biological Risks'},
-        {id : 'button_Climate_Change', text : 'Climate Crisis'}
+        {id : 'button_Animal_Welfare', text : 'Animal Welfare'},
+        {id : 'button_Global_Health_and_Development', text : 'Global Health'},
+        {id : 'button_Catastrophic_Risks', text : 'Catastrophic Risks'},
+        {id : 'button_Funds', text : 'Funds'}
         ]
-    const cause_areas = ["Animal welfare", "Human wellbeing", "Creating a better future", "Climate Change"];
+    const cause_areas = ["Animal Welfare", "Global Health and Development", "Catastrophic Risks", "Funds"];
 
+    const cause_areas_description =  [
+        {
+          "cause": "Animal Welfare",
+          "text": "If you want to reduce the suffering in the world (regardless of who is experiencing it), animal advocacy might be the right cause for you. Over one million land animals are slaughtered every hour in the US alone. We think improving animal welfare is a high-priority cause area because of its massive scale. Right now there are around 31 billion land animals being raised in factory farms so they can be slaughtered for food. These animals often experience intense suffering throughout their lives. Animal advocacy is an extremely neglected cause area. In the US, only about 3% of charitable contributions support animals and the environment, combined. Of that amount, only about 2% goes to farmed animals."
+        },
+        {
+          "cause": "Global Health and Development",
+          "text": "If you want to help people living today, supporting global health and development is a pressing matter. There are millions of people in low-income countries whose lives could be improved (and even saved, in some cases) by evidence-based, cost-effective interventions."
+        },
+        {
+          "cause": "Catastrophic Risks",
+          "text": "If you’d like to protect future generations (rather than strictly helping individuals living in the present), you should consider supporting efforts to safeguard the long-term future. Most of the work in this area involves preventing global catastrophes such as climate change, pandemics, artificial intelligence, nuclear war and making our world more resilient."
+        },
+        {
+          "cause": "Funds",
+          "text": "These funds are supporting work that touches on multiple cause areas. Donors who want to support cross-cause-area work may choose to donate to one of these funds to maximise their impact."
+        }
+      ];
+      
     
     const buttons_section = d3.select("#bubbles-container")
         .append("div")
         .attr("id", "button_section")
-        .attr("class", "flex flex-row absolute rounded-lg")
+        //.attr("class", "flex flex-row absolute rounded-lg")
+        .style("display", "flex")
         .style("justify-content", "center")
-        .style("top", "0px")
-        .style("left","0px")
-        .style("gap", "8px");  
-        //.style("width", "auto")  
-        //.style("height", "auto")    
+        .style("border-radius", "0.5rem")
+        .style("position", "relative")
+        
+        .style("top", "-950px")
+        .style("gap", "24px")
+        .style("height", "auto")    
         //.style("background-color", "gwwc-red")
     buttons.forEach(e => { 
         buttons_section//bubbles.selectAll('button')
             .append("button")
             .attr("id", e.id)
-            .attr("class", "bg-gwwc-purple p-8 hover:bg-gwwc-dark-purple active:bg-gwwc-darkest-purple rounded-lg text-white")
+            .attr("class", "bg-gwwc-purple p-10 hover:bg-gwwc-dark-purple active:bg-gwwc-darkest-purple rounded-lg text-white")
             .style("padding", "2.5px 5px")
             
             .text(e.text);
@@ -91,14 +115,14 @@ function bubbles(){
 
 
     var forceX_Separate = d3.forceX(function(d)  {
-        if(d.cause_area == 'Human wellbeing' || d.cause_area == 'Animal welfare'){
+        if(d.cause_area == 'Global Health and Development' || d.cause_area == 'Animal Welfare'){
             return width/4
         }else{
             return (width * (3/4))
         }}).strength(0.05)
 
     var forceY_Separate = d3.forceY(function(d)  {
-        if(d.cause_area == 'Human wellbeing' || d.cause_area == 'Creating a better future'){
+        if(d.cause_area == 'Global Health and Development' || d.cause_area == 'Catastrophic Risks'){
             return height/4 + 100
         }else{
             return (height * (3/4) + 100)
@@ -113,9 +137,9 @@ function bubbles(){
         .force("x", forceX_Combine)
         .force("y", forceY_Combine)
         .force("collide", d3.forceCollide(function(d) {
-            return radiusScale(d.cost_effectiveness) +10
+            return radiusScale(d.cost_effectiveness) +5
         }))
-
+        ;
 
     let separated_bubbles = false;
 
@@ -246,14 +270,87 @@ function bubbles(){
             merge_bubbles();
             
         }
-        
+
+
+
+        var causeDescriptionHere = null;
+
+
+        function cause_areas_description_out(){
+            isTransitioning = true;
+            d3.select(causeDescriptionHere).transition().duration(TRANS_DURATION)
+            .ease(d3.easeCubicInOut) 
+            .style("opacity", 0)
+            //.style("font-size", "0px")
+            .on("end", function() {
+                d3.select(this).remove();
+                causeDescriptionHere = null
+                isTransitioning = false;
+            });
+        }
         cause_areas.forEach(cause_area => { 
             d3.select("#button_" + cause_area.replace(/ /g, '_')).on('click', (event, b) => {
+                if (isTransitioning) {
+                    return; // Ignore the click if a transition is already in progress
+                  }
                 filter_cause_area(cause_area)
+                d3.select("#bubbles-container").selectAll('.cause_area-box')
+                .style('opacity', '0.0');
+
+                const textX = 40;
+                const textY = height/9;
+                const textWidth = 850;
+                console.log(causeDescriptionHere)
+                function put_text(cause_area){
+                    
+                    const foreignObject = svg
+                    .append("foreignObject")
+                    .attr("x", textX)
+                    .attr("y", textY)
+                    .attr("width", textWidth)
+                    .attr("height", 400);
+
+                    const div = foreignObject
+                        .append("xhtml:div")
+                        .style("width", "100%")
+                        .style("height", "100%")
+                        .style("word-wrap", "break-word")
+                        .style("overflow-wrap", "break-word")
+                        .style("text-align", "center")
+                        .style("font-size", "18px")
+                        .style("color", "black")
+                        .style("opacity", 0)
+                        .text(cause_areas_description.find(d => d.cause === cause_area).text);
+                    isTransitioning = true;
+                    div.transition().duration(TRANS_DURATION).ease(d3.easeCubicInOut).style("opacity", 1).on("end", function() {
+                        isTransitioning = false; // Reset the flag once the transition is complete
+                      }); // Apply a smooth easing function
+                    
+                    //.style("font-size", "18px")
+                    ;
+                    causeDescriptionHere = div.node();
+                }
+                if (causeDescriptionHere) {
+                    // Fade out the text element
+                    cause_areas_description_out()
+                    put_text(cause_area)  
+                    //put_text(cause_area)
+                  } else {
+                    put_text(cause_area)
+
+                    
+                  }
+           
             });
             
         });
-        d3.select("#button_cause").on('click', () => {
+        d3.select("#button_separation").on('click', () => {
+            if (isTransitioning) {
+                return; // Ignore the click if a transition is already in progress
+              }
+            if (causeDescriptionHere) {
+                // Fade out the text element
+                cause_areas_description_out()}
             var_datapoints = original_datapoints;
             circles = join_circles(var_datapoints);
             join_defs();  
@@ -263,6 +360,12 @@ function bubbles(){
             });
     
         d3.select("#combine").on('click', () => {
+            if (isTransitioning) {
+                return; // Ignore the click if a transition is already in progress
+              }
+            if (causeDescriptionHere) {
+            // Fade out the text element
+                cause_areas_description_out()}  
             var_datapoints = original_datapoints;
             circles = join_circles(var_datapoints);
             join_defs();            
@@ -284,5 +387,7 @@ function bubbles(){
         simulation.nodes(var_datapoints).on('tick', ticked)
         
     }
+
+    //add text
 
 }
